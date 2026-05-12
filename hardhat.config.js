d55eea0e1340bd00011d5b9db6c461a53f5397b3
kajko25 <112957382+Kajko25@users.noreply.github.com>
2026-05-12 15:21:48 +0200

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
