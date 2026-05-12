7f6b9258fdb2411e85fcf7f763d3c950992d3725
kajko25 <112957382+Kajko25@users.noreply.github.com>
2026-05-12 16:28:17 +0200

    Update ERC20Token.sol

--- base-staking-protocol/contracts/ERC20Token.sol
+++ base-staking-protocol/contracts/ERC20Token.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract BaseToken {
+    string public name = "BaseStake Token";
+    string public symbol = "BST";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply = 1_000_000 * 10**18;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "no balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        return true;
+    }
+}
